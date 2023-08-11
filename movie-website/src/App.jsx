9b8229217baa9098e0dc3f4f1892a2968cb71e90
AndreJarl <104331025+AndreJarl@users.noreply.github.com>
2023-08-11 18:20:20 +0800

    added components

--- movie-website/src/App.jsx
+++ movie-website/src/App.jsx
@@ -1,34 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
- 
-  useEffect(() => {
-    fetchdata();
-  },[]);
-
-
-  const url = "https://api.consumet.org/manga/mangadex/info/259dfd8a-f06a-4825-8fa6-a2dcd7274230";
-  const fetchdata = async () => {
-   try {
-   const { data } = await axios.get(url);
-   setMovies(data)
-   } catch (err) {
-   throw new Error(err.message);
-   }
-  };
-  
-  console.log(movies);
-  
-
   return (
     <>
-     
+   <Navbar/>
+   <Hero/>
+   <Footer/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
